1ab698fc219a771399b51ea0ba50939e35bb4af7
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-12 23:33:13 +0300

    Update welcome page layout and add Google Fonts package

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -9,11 +10,24 @@ class WelcomePage extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('/lib/assets/images/background.jpg'),
+            image: AssetImage('lib/assets/images/background.jpg'),
             fit: BoxFit.cover, // Adjust how the image fits the container
           ),
         ),
-        child: Center(),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'SLUMBERNAUT',
+                style: GoogleFonts.roboto(fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white,),
+              ),
+              Text(
+                'Your personal sleep tracker',
+                style: GoogleFonts.roboto(fontSize: 15, color: Colors.white,),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
