43a743fe6994c4f59b876f7591814f1a1546d518
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-13 01:22:58 +0300

    Refactor welcome page layout, update container widget, and add Google button

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -22,20 +22,20 @@ class WelcomePage extends StatelessWidget {
           ),
           SingleChildScrollView(
             // Use SingleChildScrollView to handle overflow
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      FadeInUp(
-                        duration: Duration(milliseconds: 1000),
-                        child: Text(
+            child: FadeInUp(
+              duration: Duration(milliseconds: 1000),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           'SLUMBERNAUT',
                           style: GoogleFonts.roboto(
                             fontWeight: FontWeight.bold,
@@ -43,44 +43,38 @@ class WelcomePage extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      FadeInUp(
-                        duration: Duration(milliseconds: 1300),
-                        child: Text(
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
                           'Your personal sleep tracker app',
                           style: GoogleFonts.roboto(
                             fontSize: 15,
                             color: Colors.white,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.teal[400],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(70),
-                      topRight: Radius.circular(70),
+                      ],
                     ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(30),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 40,
-                        ),
-                        FadeInUp(
-                          duration: Duration(milliseconds: 1400),
-                          child: Container(
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.teal[400],
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(70),
+                        topRight: Radius.circular(70),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Container(
                             padding: EdgeInsets.all(20),
                             decoration: BoxDecoration(
                                 color: Colors.teal[200],
@@ -128,79 +122,84 @@ class WelcomePage extends StatelessWidget {
                               ),
                             ]),
                           ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        FadeInUp(
-                          duration: Duration(milliseconds: 1500),
-                          child: TextButton(
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextButton(
                             onPressed: () {},
                             child: Text(
                               'Forgot Password?',
                               style: TextStyle(color: Colors.white),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeInUp(duration: Duration(milliseconds: 1600), child: Container(
-                          height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 50),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.orange,
+                          SizedBox(
+                            height: 20,
                           ),
-                          child: Center(
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
+                          Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 50),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.orange,
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                ),
                               ),
                             ),
                           ),
-                        ),),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeInUp(duration: Duration(milliseconds: 1700), child: Text(
-                          'Or connect using',
-                          style: TextStyle(color: Colors.white),
-                        ),),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: FadeInUp(duration: Duration(milliseconds: 1800), child: Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.blue,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    'Google',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 17,
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Or connect using',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue,
+                                  ),
+                                  child: Center(
+                                    child: Row(
+                                      children: [
+                                        Image.asset('lib/assets/images/google.png'),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          'Google',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
                               ),
-                            ),),
-                          ],
-                        )
-                      ],
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
