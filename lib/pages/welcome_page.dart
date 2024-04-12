4f5d2576266c68a7863e8ac28e3f5dd57a63d2b3
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-13 01:56:41 +0300

    Add Firebase initialization and imports to main.dart and welcome_page.dart

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key});
