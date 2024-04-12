2661c247383b066477dbe8f2c7e792ff69cb218f
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-13 00:25:45 +0300

    Refactor welcome page layout, add Google Fonts package, and create a container widget

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -51,15 +51,71 @@ class WelcomePage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 20,
+              ),
               Expanded( // Expanded widget to fill the remaining space
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(70),
+                      topRight: Radius.circular(70),
                     ),
                   ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 40,),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [BoxShadow(
+                            color: Colors.orange,
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                          ),]
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey)),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Email',
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey)),
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                decoration: InputDecoration(
+                                  hintText: 'Password',
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                          ]
+                        ),
+                      )
+                    ],
+                  ),
+                ),
                 ),
               ),
             ],
