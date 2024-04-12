d9f3a8b6f4148566ff6a6b30454e13f91c8f2876
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-13 00:06:04 +0300

    Refactor welcome page layout and add Google Fonts package

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+  const WelcomePage ({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.symmetric(vertical: 30),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/assets/images/background.jpg'),
-            fit: BoxFit.cover, // Adjust how the image fits the container
+      body: Stack( // Wrap your Column with a Stack
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 30),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/assets/images/background.jpg'),
+                fit: BoxFit.cover, // Adjust how the image fits the container
+              ),
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
                 height: 90,
@@ -45,22 +48,23 @@ class WelcomePage extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
-                    Expanded(
-                        child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30),
-                        ),
-                      ),
-                    )),
                   ],
+                ),
+              ),
+              Expanded( // Expanded widget to fill the remaining space
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
