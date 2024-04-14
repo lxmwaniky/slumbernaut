577256f0f085669e29ae494d0f26a58d6d857401
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-04-14 14:42:19 +0300

    Add state to HomePage for tracking sleep duration
    - Change HomePage from Stateless to Stateful widget
    - Add sleepStartTime, sleepEndTime, and sleepDuration variables to _HomePageState

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,19 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String user;
 
   const HomePage({Key? key, required this.user}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime? sleepStartTime;
+  DateTime? sleepEndTime;
+  Duration? sleepDuration;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +30,7 @@ class HomePage extends StatelessWidget {
     } else {
       greeting = 'Good Evening';
     }
+
     return Scaffold(
       body: Stack(
         children: [
@@ -50,7 +60,7 @@ class HomePage extends StatelessWidget {
                     children: [
                       FadeInLeft(
                         child: Text(
-                          '$greeting, $user!',
+                          '$greeting, ${widget.user}!',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 35,
@@ -61,6 +71,35 @@ class HomePage extends StatelessWidget {
                       SizedBox(
                         height: 10,
                       ),
+                      _buildSleepButton(
+                        text: 'Go to Sleep',
+                        onPressed: () {
+                          setState(() {
+                            sleepStartTime = DateTime.now();
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      _buildSleepButton(
+                        text: 'Wake Up',
+                        onPressed: () {
+                          setState(() {
+                            sleepEndTime = DateTime.now();
+                            if (sleepStartTime != null) {
+                              sleepDuration = sleepEndTime!.difference(sleepStartTime!);
+                            }
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      if (sleepDuration != null)
+                        Text(
+                          'You slept for ${sleepDuration!.inHours} hours and ${sleepDuration!.inMinutes.remainder(60)} minutes',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
                     ],
                   ),
                 ),
@@ -68,6 +107,29 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildSleepButton({required String text, required VoidCallback onPressed}) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 50,
+        width: 150,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.orange,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
       ),
     );
   }
